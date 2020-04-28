dce24a2fc1056863e0f666c5ec1953d0b087d306
Zafar Ali Khan <33575935+zakonweb@users.noreply.github.com>
2020-04-29 04:47:08 +0500

    Add files via upload

---
+++ While End While - Format 2 - V1/Format 2 V1 PN/Module1.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim PN, Largest, Total, Count As Integer
+        Dim AVG As Single
+
+        PN = 0
+        Largest = -1000
+        Total = 0
+        Count = 0
+        AVG = 0
+
+        Do
+            PN = Console.ReadLine()
+            Total = Total + PN
+            Count = Count + 1
+
+            If PN > Largest Then
+                Largest = PN
+            End If
+
+        Loop Until (PN = -1)
+        AVG = Total / Count
+        Console.WriteLine(AVG)
+        Console.WriteLine(Largest)
+        Console.ReadKey()
+    End Sub
+
+End Module
