a0827fef2b30c6c817f76c7a653fcd2668c07a37
Zafar Ali Khan <33575935+zakonweb@users.noreply.github.com>
2020-04-29 04:37:13 +0500

    Add files via upload

---
+++ Arrays - OL/Array Questions 1 to 3 - PP/Module1.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Names(5) As String
+        Dim Test1(5) As Integer
+        Dim Test2(5) As Integer
+        Dim Test3(5) As Integer
+        Dim Total(5) As Integer
+
+        Dim Highest, Avg, i, GT As Integer
+
+        For i = 1 To 5
+            Names(i) = Console.ReadLine
+            Test1(i) = Console.ReadLine
+            Test2(i) = Console.ReadLine
+            Test3(i) = Console.ReadLine
+            Total(i) = Test1(i) + Test2(i) + Test3(i)
+
+            GT = GT + Total(i)
+        Next i
+
+        For i = 1 To 5
+            Console.WriteLine(Names(i) & ", " & Total(i))
+        Next i
+
+        Avg = GT / 5
+        Console.WriteLine("Average :" & Avg)
+
+        Highest = -1000
+        For i = 1 To 5
+            If Total(i) > Highest Then Highest = Total(i)
+        Next i
+
+        Console.WriteLine("Highest = " & Highest)
+
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ Arrays - OL/SimpleAarray - Flag use/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim i As Integer
+        Dim searchName, names(5) As String
+        Dim isFound As Boolean
+        isFound = False
+
+        For i = 1 To 5
+            names(i) = Console.ReadLine
+        Next
+
+        searchName = Console.ReadLine
+        For i = 1 To 5
+            If searchName = names(i) Then
+                isFound = True
+                Console.WriteLine("Found @" & i)
+            End If
+        Next.
+        If isFound = False Then Console.WriteLine("Not FOund.")
+        Console.ReadKey()
+
+    End Sub
+
+End Module
